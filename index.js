af73ddce23ed7560e457d9960fc59cb6f84b0d18
sankethsura <110083276+sankethsura@users.noreply.github.com>
2023-05-20 12:37:40 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`running socket ${socket.id} `);
+  socket.on("send_message", (data) => {
+    socket.broadcast.emit("recieve_message", data);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("running");
+});
